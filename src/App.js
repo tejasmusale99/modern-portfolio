78b394781d8cf2a5c0771e6de700b81858772d36
Tejas Musale <tejasmusale99@gmail.com>
2024-09-19 22:48:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return(
+        <>
+        <h1>hey! Lets get Started
+        </h1>
+        </>
+    )
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
+export default App;
